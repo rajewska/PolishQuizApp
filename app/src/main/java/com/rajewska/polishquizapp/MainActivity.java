46f8fff4419de9fe1e760c66a313fbe39dda899c
Zuzanna Rajewska <zrajewska@gmail.com>
2018-01-28 10:29:29 -0200

    Fixed java, added reset button

--- app/src/main/java/com/rajewska/polishquizapp/MainActivity.java
+++ app/src/main/java/com/rajewska/polishquizapp/MainActivity.java
@@ -1,12 +1,19 @@
 package com.rajewska.polishquizapp;
 
+import android.content.Context;
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +22,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if (v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int) event.getRawX(), (int) event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    if (imm != null) {
+                        imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                    }
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event);
     }
 
     int score = 0;
@@ -22,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
     public void showMyResult(View view) {
         EditText nameField = (EditText) findViewById(R.id.name_field);
         String name = nameField.getText().toString();
-        Log.v("Main Activity", "Name: " + name);
+
 
         RadioButton question1 = findViewById(R.id.answer_1);
         RadioButton question2 = findViewById(R.id.answer_2);
@@ -61,8 +88,6 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreSummary = (TextView) findViewById(R.id.score_display);
         scoreSummary.setText(scoreMessage);
 
-        score = 0;
-
 
     }
 
@@ -85,7 +110,57 @@ public class MainActivity extends AppCompatActivity {
         }
         return scoreMessage;
 
+
     }
+
+    public void reset (View view) {
+        score = 0;
+
+        EditText textField = (EditText) findViewById(R.id.name_field);
+        textField.setText(null);
+
+
+        RadioGroup radio1 = (RadioGroup) findViewById(R.id.radio1);
+        radio1.clearCheck();
+
+        RadioGroup radio2 = (RadioGroup) findViewById(R.id.radio2);
+        radio2.clearCheck();
+
+        RadioGroup radio3 = (RadioGroup) findViewById(R.id.radio3);
+        radio3.clearCheck();
+
+        RadioGroup radio4 = (RadioGroup) findViewById(R.id.radio4);
+        radio4.clearCheck();
+
+        RadioGroup radio5 = (RadioGroup) findViewById(R.id.radio5);
+        radio5.clearCheck();
+
+        RadioGroup radio6 = (RadioGroup) findViewById(R.id.radio6);
+        radio6.clearCheck();
+
+        RadioGroup radio7 = (RadioGroup) findViewById(R.id.radio7);
+        radio7.clearCheck();
+
+        RadioGroup radio8 = (RadioGroup) findViewById(R.id.radio8);
+        radio8.clearCheck();
+
+        RadioGroup radio9 = (RadioGroup) findViewById(R.id.radio9);
+        radio9.clearCheck();
+
+        RadioGroup radio10 = (RadioGroup) findViewById(R.id.radio10);
+        radio10.clearCheck();
+
+        ScrollView scroll = (ScrollView) findViewById(R.id.scroll);
+        scroll.fullScroll(ScrollView.FOCUS_UP);
+        scroll.setFocusableInTouchMode(true);
+
+        TextView scoreDisplay = (TextView) findViewById(R.id.score_display);
+        scoreDisplay.setText(null);
+
+
+    }
+
+
 
 }
 
